32fa4a50f30496dee4183f36c17f5d6111c8203b
lucikkjl <lucirodrigues734@gmail.com>
2026-03-27 19:21:15 -0300

    commit

--- Front-End/eslint.config.js
+++ Front-End/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import jsxA11y from "eslint-plugin-jsx-a11y"; // 1. Importando o plugin
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "jsx-a11y": jsxA11y, // 2. Registrando o plugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules, // 3. Ativando as regras recomendadas de acessibilidade
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
