3c5d85ae0642f0c2d0e3f02a2f846d7ebd569204
lucikkjl <lucirodrigues734@gmail.com>
2026-03-27 19:45:16 -0300

    alt

--- Front-End/eslint.config.js
+++ Front-End/eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import jsxA11y from "eslint-plugin-jsx-a11y"; // 1. Importando o plugin
+import jsxA11y from "eslint-plugin-jsx-a11y"; 
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -17,16 +17,22 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "jsx-a11y": jsxA11y, // 2. Registrando o plugin
+      "jsx-a11y": jsxA11y, 
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules, // 3. Ativando as regras recomendadas de acessibilidade
+      ...jsxA11y.configs.recommended.rules, //
       "react-refresh/only-export-components": [
-        "warn",
+        "off",
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
+      
+      "jsx-a11y/heading-has-content": "off",
+      "jsx-a11y/anchor-has-content": "off",
+      
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-require-imports": "off",
     },
   }
 );
